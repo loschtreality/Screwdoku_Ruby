1e74dd79370655c3d9907bf0fca163a28e5686ab
App Academy Student <student@appacademy.io>
2016-09-15 14:04:35 -0700

    Fix multiple spelling mistakes in Sudoku and Board. Fixed file reading and spacing in render and sudoku text.

--- board.rb
+++ board.rb
@@ -1,4 +1,5 @@
 require_relative "tile"
+require 'byebug'
 
 class Board
   attr_reader :grid
@@ -7,14 +8,14 @@ class Board
     @grid = Array.new(9) do
       Array.new(9) { Tile.new(0) }
     end
-    grid
+    @grid
   end
 
   def self.from_file(filename)
-    rows = File.readlines("filename").map(:chomp)
+    rows = File.readlines(filename).map(&:chomp)
     tiles = rows.map do |row|
-      nums = row.split("").map { |char| parseInt(char) }
-      nums.map { |num| Tle.new(num) }
+      nums = row.split("").map { |char| Integer(char) }
+      nums.map { |num| Tile.new(num) }
     end
 
     self.new(tiles)
@@ -32,7 +33,7 @@ class Board
   def []=(pos, value)
     x, y = pos
     tile = grid[x][y]
-    tile.value = new_value
+    tile.value = value
   end
 
   def columns
@@ -40,7 +41,7 @@ class Board
   end
 
   def render
-    puts "(0..8).to_a.join(" ")"
+    puts "  #{(0..8).to_a.join(" ")}"
     grid.each_with_index do |row, i|
       puts "#{i} #{row.join(" ")}"
     end
@@ -51,7 +52,10 @@ class Board
     grid.size
   end
 
-  alias_method :rows, :size
+  def rows
+    @grid
+  end
+
 
   def solved?
     rows.all? { |row| solved_set?(row) } &&
